28e72e6f836b3cd658077042b8a6e39e16f0c60e
joe byrne <joebyrne@joes-MacBook-Pro.local>
2021-07-15 19:47:06 +0100

    jQuery board set up

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log('hello world');
+
+$( document ).ready(function() {
+
+    $('[piece]').each(function() {
+        let player = $(this).attr('player');
+        let piece = $(this).attr('piece');
+
+        backgroundColour = $(this).css('background-color')
+        $(this).css("background" , "url(./pieces/" + player + "/" + piece + ".svg)").css("background-color", backgroundColour);
+
+        // if ($(this).hasClass(dark)) {
+        //     $(this).css("background-color" , "grey");
+        // }
+    })
+
+});
+
